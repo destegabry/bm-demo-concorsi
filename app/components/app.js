64154cda06800efefa2dc04449732f2977c99c7a
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2015-10-13 10:59:45 +0200

    Firebase login/register/forgot

--- app/components/app.js
+++ app/components/app.js
@@ -13,9 +13,11 @@ angular
     'ngCookies',
     'ui.router', 
     'dsg.loadingCounter', 
-    'pascalprecht.translate'
+    'pascalprecht.translate',
+    'firebase'
   ])
   .constant('LANG_COOKIE_KEY', 'lang_cookie')
+  .constant('FIREBASE_APP', new Firebase('https://bm-demo-concorsi.firebaseio.com'))
   .controller('AppCtrl', function ($scope, $cookies, $translate, AVAILABLE_LANGUAGES, LANG_COOKIE_KEY) {
     
     $scope.changeLanguage = function (lang) {
--- app/components/public/user.js
+++ app/components/public/user.js
@@ -1,18 +1,41 @@
 "use strict";
 
 angular.module('bmDemoConcorsiApp')
-  .controller('UserCtrl', function ($scope, User) {
+  .controller('UserCtrl', function ($scope, $firebaseAuth, User, FIREBASE_APP) {
     $scope.user = User;
+    var authObj = $firebaseAuth(FIREBASE_APP);
 
     $scope.login = function () {
-
+      authObj.$authWithPassword({
+        email: $scope.user.email,
+        password: $scope.user.password
+      }).then(function (authData) {
+        $scope.user = {};
+        // redirect su applicazione
+      }).catch(function(error) {
+        $scope.error = error;
+      });
     };
     
     $scope.register = function () {
-
+      authObj.$createUser({
+        email: $scope.user.email,
+        password: $scope.user.password
+      }).then(function(userData) {
+        $scope.login();
+      }).catch(function(error) {
+        $scope.error = error;
+      });
     };
     
     $scope.forgot = function () {
-
+      authObj.$resetPassword({
+        email: $scope.user.email
+      }).then(function() {
+        $scope.user = {};
+        $scope.message = 'PASSWORD_RESET_SUCCESS';
+      }).catch(function(error) {
+        $scope.error = error;
+      });
     };
   });
