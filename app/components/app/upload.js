2adb804db02aefa40bd8ff03a30ab8c89b00c3aa
Gabriele Destefanis <gabriele.destefanis@gmail.com>
2015-10-15 10:20:09 +0200

    Added push on firebase array

--- app/components/app/upload.js
+++ app/components/app/upload.js
@@ -1,4 +1,19 @@
 angular.module('bmDemoConcorsiApp')
-  .controller('UploadCtrl', function($scope) {
+  .controller('UploadCtrl', function($scope, $filter, $firebaseArray, FIREBASE_APP, UserService, MessageService) {
+    $scope.uploadText = '';
+    var list = $firebaseArray(FIREBASE_APP);
     
+    $scope.upload = function () {
+      list.$add({
+        text: $scope.uploadText,
+        type: 'upload-submit',
+        uid: UserService.isLogged().uid
+      })
+        .then(function() {
+          $scope.uploadText = '';
+          MessageService.info($filter('translate')('UPLOAD_SUCCESS'));
+        }, function(error) {
+          MessageService.error(error.code);
+        });
+    };
   });
